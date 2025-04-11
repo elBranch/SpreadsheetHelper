b4cf7de4cad6e66581605b29ae41943f514a8545
Will Branch <126515826+elBranch@users.noreply.github.com>
2025-04-11 10:14:20 -0400

    update license

--- Internal/CellStyleBuilder.cs
+++ Internal/CellStyleBuilder.cs
@@ -1,4 +1,17 @@
-﻿using NPOI.SS.UserModel;
+﻿// A .NET library to simplify reading and writing Excel files using NPOI.
+// Copyright (C) 2025 Will Branch
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program. If not, see
+// <https://www.gnu.org/licenses/>.
+
+using NPOI.SS.UserModel;
 using HorizontalAlignment = NPOI.SS.UserModel.HorizontalAlignment;
 using VerticalAlignment = NPOI.SS.UserModel.VerticalAlignment;
 
